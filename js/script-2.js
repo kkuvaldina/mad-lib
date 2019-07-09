12ed66322ae51900d6be21f14dfc27e8ff43d129
Kseniia <kuvaldina.ks@gmail.com>
2019-07-08 18:28:31 -0600

    final

---
+++ js/script-2.js
@@ -0,0 +1,14 @@
+var noun1 = prompt("First noun");
+var noun2 = prompt("Second noun");
+var verb1 = prompt("First verb");
+var verb2 = prompt("Second verb");
+var adj1 = prompt("First adjective");
+var adj2 = prompt("Second adjective");
+
+
+document.write("A salt seller used to carry the salt bag on his donkey to the market every day. On the way they had to cross a " + noun1 + ". One day the donkey suddenly " + verb1 + " the stream and the salt bag also fell into the water. The salt dissolved in the water and hence the bag became very " + adj1 + " to carry. The donkey was " + adj2 + ". Then the donkey started to " + verb2 + " the same trick every day. The salt seller came to understand the trick and decided to teach a lesson to it. The next day he loaded a cotton bag on the donkey. Again it played the same trick hoping that the cotton bag would be still become lighter. But the dampened cotton became very heavy to carry and the donkey suffered. It learnt a lesson. It didn’t play the trick anymore after that day, and the " + noun2 + " was happy.");
+   
+
+
+
+
---
+++ js/script.js
@@ -0,0 +1,13 @@
+var noun1 = prompt("First noun");
+var noun2 = prompt("Second noun");
+var verb1 = prompt("First verb");
+var verb2 = prompt("Second verb");
+var adj1 = prompt("First adjective");
+var adj2 = prompt("Second adjective");
+
+
+
+document.write("An " + adj1 + " man lived in the village. He was one of the most unfortunate people in the world. The whole village was tired of him; he was always " + adj2 + ", he constantly complained and was always in a bad mood. The longer he lived, the more bile he was becoming and the more poisonous were his words. " + noun1 + " avoided him, because his misfortune became contagious. It was even unnatural and insulting to be happy next to him. He created the feeling of unhappiness in others. But one day, when he turned eighty years old, an incredible thing happened. Instantly everyone started hearing the rumour: “An Old Man is happy today, he doesn’t complain about anything, " + verb1 + ", and even his " + noun2 + " is freshened up.” The whole village gathered together. The old man was asked: What happened to you? “Nothing special. Eighty years I’ve been chasing happiness, and it was useless. And then I decided to live without happiness and just " + verb2 + " life. That’s why I’m happy now.” – An Old Man.");
+
+
+
